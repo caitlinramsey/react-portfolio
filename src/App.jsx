666cbb8351e677e9a1e4bea26186f939bc132d00
Caitlin <caitlinramsey@outlook.com>
2023-08-04 15:17:34 -0400

    added logic to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Project from './components/project/Project'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Project />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default App;
