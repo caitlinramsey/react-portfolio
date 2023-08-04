1f7cbdce666c76198668ae18ed152e3b3878389e
Caitlin <caitlinramsey@outlook.com>
2023-08-04 15:42:46 -0400

    added logic to About.jsx

---
+++ src/components/about/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './about.css';
+import aboutMe from '../../assets/me-and-boys.jpg';
+
+
+function About() {
+    return (
+        <section id='about-me'>
+            <h1>About Me</h1>
+
+            <div className='container about_me_container'>
+                <div className='about_me'>
+                    <div className='about_me_image'>
+                        <img src={aboutMe} alt='A photo of me and my 2 dogs' />
+                    </div>
+                </div>
+
+                <div className='about_me_content'>
+                    <p>
+                        I graduted from Cedar Crest College in May 2019 with a Bachelor of Science in Environmental Conservation and a Bachelor of Arts in New Media. 
+                        <br/>
+                        I am an organized, hard-working individual..
+                    </p>
+
+                    <a href='#contact' className='btn btn-primary about_btn'>
+                        Contact Me
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default About;
