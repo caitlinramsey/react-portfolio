b54e11e1458eae5c505ba7f29582ec9b2a76681a
Caitlin <caitlinramsey@outlook.com>
2023-08-06 21:50:39 -0400

    worked on homepage styling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,19 @@
 import Resume from "./Resume";
 import "./home.css";
+import background from '../../assets/jaguar.jpg'
+// import React from 'react';
 
 export default function Home() {
   return (
+    <div style={{
+      backgroundImage: `url(${background})`,
+      backgroundRepeat: 'no-repeat',
+      height: '1000px',
+      backgroundPosition: '5px',
+      backgroundSize: '3000px',
+      margin: '0px',
+      opacity: '0.6'
+    }}>
     <header id="header">
       <div className="container header_container">
         <h4>Hello I'm</h4>
@@ -11,5 +22,6 @@ export default function Home() {
         <Resume />
       </div>
     </header>
+    </div>
   );
 }
