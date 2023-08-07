3397f3a70e427cea3beba90bba1dc3dc13bc0b4c
Caitlin <caitlinramsey@outlook.com>
2023-08-07 14:48:18 -0400

    worked on styling font on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,6 @@ import { Outlet } from 'react-router-dom';
 import Navigation from './components/nav/Navigation';
 import Footer from './components/footer/Footer'
 
-// import Header from './components/header/Header'
-// // import About from './components/about/About'
-// // import Experience from './components/experience/Experience'
-// // import Project from './components/project/Project'
-// // import Services from './components/services/Services'
-// // import Contact from './components/contact/Contact'
-// import Footer from './components/footer/Footer'
-
-// const App = () => {
-//     return (
-//         <>
-//             <Header />
-//             <Navigation />
-//             <About />
-//             <Experience />
-//             <Project />
-//             <Services />
-//             <Contact />
-//             <Footer />
-//         </>
-//     )
-// }
-
 function App() {
   return (
     <>
