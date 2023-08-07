35e72921be1400bbab64dd872d9c2ba992407b43
Caitlin <caitlinramsey@outlook.com>
2023-08-06 20:06:41 -0400

    worked on nav styling

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -1,23 +1,53 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import './navigation.css';
-import {BiHomeAlt, BiUser, BiCodeCurly, BiSolidContact} from 'react-icons/bi';
-import {FaServicestack} from 'react-icons/fa';
-import {GrStackOverflow} from 'react-icons/gr';
-import {Link} from 'react-scroll';
+// import {BiHomeAlt, BiUser, BiCodeCurly, BiSolidContact} from 'react-icons/bi';
+// import {FaServicestack} from 'react-icons/fa';
+// import {GrStackOverflow} from 'react-icons/gr';
+// import {Link} from 'react-scroll';
 
-function Navigation () {
-    const [activeNavigation, setActiveNavigation] = useState('#')
+// function Navigation () {
+//     const [activeNavigation, setActiveNavigation] = useState('#')
+//     return (
+//         <nav>
+//             <Link to='header' spy={true} smooth={true} duration={500} activeClass='active'><BiHomeAlt/></Link>
+//             <Link to='about' spy={true} smooth={true} duration={500} activeClass='active'><BiUser/></Link>
+//             <Link to='experience' spy={true} smooth={true} duration={500} activeClass='active'><BiCodeCurly/></Link>
+//             <Link to='project' spy={true} smooth={true} duration={500} activeClass='active'><GrStackOverflow/></Link>
+//             <Link to='services' spy={true} smooth={true} duration={500} activeClass='active'><FaServicestack/></Link>
+//             <Link to='contact' spy={true} smooth={true} duration={500} activeClass='active'><BiSolidContact/></Link>
+//         </nav>
+//     )
+// }
+
+function Navigation(props) {
+    const tabs = ['About', 'Experience', 'Portfolio', 'Services', 'Contact'];
     return (
-        <nav>
-            <Link to='header' spy={true} smooth={true} duration={500} activeClass='active'><BiHomeAlt/></Link>
-            <Link to='about' spy={true} smooth={true} duration={500} activeClass='active'><BiUser/></Link>
-            <Link to='experience' spy={true} smooth={true} duration={500} activeClass='active'><BiCodeCurly/></Link>
-            <Link to='project' spy={true} smooth={true} duration={500} activeClass='active'><GrStackOverflow/></Link>
-            <Link to='services' spy={true} smooth={true} duration={500} activeClass='active'><FaServicestack/></Link>
-            <Link to='contact' spy={true} smooth={true} duration={500} activeClass='active'><BiSolidContact/></Link>
-        </nav>
-    )
+        <div className="tabs">
+      <ul className="nav nav-tabs">
+        {tabs.map((tab) => (
+          <li
+            className={
+              props.currentPage === tab ? "nav-item is-active" : "nav-item"
+            }
+            key={tab}
+          >
+            <a
+              href={"#" + tab.toLowerCase()}
+              // Whenever a tab is clicked on,
+              // the current page is set through the handlePageChange props.
+              onClick={() => props.handlePageChange(tab)}
+              className={
+                props.currentPage === tab ? "nav-link active" : "nav-link"
+              }
+            >
+              {tab}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Navigation;
