d096ff5d786ad8e96b75cb40b70283fe48aee319
Caitlin <caitlinramsey@outlook.com>
2023-09-07 23:56:09 -0400

    worked on experience page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,22 +4,22 @@ import './about.css';
 
 function About() {
     return (
-        <section id='about_me'>
-            <h1>About Me</h1>
+        <section id='about-me' className='about-me pt-5'>
+            <h1 className='text-center'>About Me</h1>
 
-            <div className='container about_me_container'>
-                <div className='about_me'>
-                    <div className='about_me_image'></div>
+            <div className='container about-me-container'>
+                <div className='about-me'>
+                    <div className='about-me-image'></div>
                 </div>
 
-                <div className='about_me_content'>
+                <div className='about-me-content'>
                     <p>
                         I graduated from Cedar Crest College in May 2019 with a Bachelor of Science in Environmental Conservation and a Bachelor of Arts in New Media. I also graduated from a 6 month course from the University of North Carolina at Chapel Hill with a certificate in Full-Stack Development. I am one class away from completing a graduate certificate in Fisheries Management from Oregon State University.
                         <br/><br/>
                         I am an organized, hard-working, dependable individual. I am very passionate about being creative and problem-solving. 
                     </p>
 
-                    <a href='../Contact' className='btn about_btn'>
+                    <a href='../Contact' className='btn about-btn'>
                         Contact Me
                     </a>
                 </div>
