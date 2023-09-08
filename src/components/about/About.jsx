5743773855f7850d07ba0ca16d25982c93150e67
Caitlin <caitlinramsey@outlook.com>
2023-09-08 16:09:46 -0400

    worked on about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import './about.css';
-
+import background from '../../assets/boysbw.png'
+import Container from 'react-bootstrap/Container';
 
 function About() {
     return (
+        <div style={{
+            backgroundImage: `url(${background})`,
+            backgroundRepeat: 'no-repeat',
+            width:'100vw',
+            backgroundSize: '3000px',
+            marginLeft: '-16px',
+          }}>
         <section id='about-me' className='about-me pt-5'>
             <h1 className='text-center'>About Me</h1>
 
-            <div className='container about-me-container'>
+            <Container className='about-me-container'>
                 <div className='about-me'>
                     <div className='about-me-image'></div>
                 </div>
 
-                <div className='about-me-content'>
+                <div className='about-me-content text-center'>
                     <p>
                         I graduated from Cedar Crest College in May 2019 with a Bachelor of Science in Environmental Conservation and a Bachelor of Arts in New Media. I also graduated from a 6 month course from the University of North Carolina at Chapel Hill with a certificate in Full-Stack Development. I am one class away from completing a graduate certificate in Fisheries Management from Oregon State University.
                         <br/><br/>
                         I am an organized, hard-working, dependable individual. I am very passionate about being creative and problem-solving. 
                     </p>
 
-                    <a href='../Contact' className='btn about-btn'>
+                    <a href='../Contact' className='btn about-btn fw-bold'>
                         Contact Me
                     </a>
                 </div>
-            </div>
+            </Container>
         </section>
+        </div>
     );
 }
 
