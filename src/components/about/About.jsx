c758e3ae9b493ef347fb881862b8b3a0684c1a75
Caitlin <caitlinramsey@outlook.com>
2023-10-31 11:44:35 -0400

    updated resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,7 +18,7 @@ function About() {
                     <p>
                         I graduated from Cedar Crest College in May 2019 with a Bachelor of Science in Environmental Conservation and a Bachelor of Arts in New Media. In August of 2023, I graduated from a 6-month course through the University of North Carolina at Chapel Hill with a certificate in Full-Stack Development. I am one class away from completing a graduate certificate in Fisheries Management through Oregon State University.
                         <br/><br/>
-                        I am an organized, hard-working, dependable individual. I am very passionate about being creative and problem-solving. I enjoy the challenge of learning new programming languages and techniques.
+                        I am an organized, hard-working, and dependable individual. I am very passionate about being creative and problem-solving. I enjoy the challenge of learning new programming languages and techniques.
                     </p>
                 </div>
             </Container>
